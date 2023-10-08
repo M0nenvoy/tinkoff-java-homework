451b752e2551eb714dbbee23386e0f2bc9204829
M0nenvoy <goldseeker68191@gmail.com>
2023-10-08 12:01:06 +0400

    Убраны неиспользованные методы

--- src/test/java/edu/hw1/Task8Test.java
+++ src/test/java/edu/hw1/Task8Test.java
@@ -4,11 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
 
 public class Task8Test {
     Task8 task;
@@ -16,21 +11,6 @@ public class Task8Test {
     @BeforeEach
     void initializeTask() {
         this.task = new Task8();
-    }
-
-    private static Stream<Arguments> provideBoards() {
-        return Stream.of(
-            Arguments.of((Object) new int[][] {
-                new int[] {0, 0, 0, 1, 0, 0, 0, 0},
-                new int[] {0, 0, 0, 0, 0, 0, 0, 0},
-                new int[] {0, 1, 0, 0, 0, 1, 0, 0},
-                new int[] {0, 0, 0, 0, 1, 0, 1, 0},
-                new int[] {0, 1, 0, 0, 0, 1, 0, 0},
-                new int[] {0, 0, 0, 0, 0, 0, 0, 0},
-                new int[] {0, 1, 0, 0, 0, 0, 0, 1},
-                new int[] {0, 0, 0, 0, 1, 0, 0, 0},
-            }, true)
-        );
     }
 
     @DisplayName("knightBoardCapture - Истина")
