398b334a93e1dad4e01633443f2a568578a21258
M0nenvoy <goldseeker68191@gmail.com>
2023-10-21 18:24:03 +0400

    Исправление стиля

--- src/main/java/edu/hw2/Task3/DefaultConnectionManager.java
+++ src/main/java/edu/hw2/Task3/DefaultConnectionManager.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class DefaultConnectionManager implements ConnectionManager {
     private static final float FAULTY_CONNECTION_CHANCE = 0.5f;
     private final float faultyChance;
+
     DefaultConnectionManager(float faultyChance) {
         this.faultyChance = faultyChance;
     }
